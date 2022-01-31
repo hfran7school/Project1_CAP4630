a56a50ff08cdf099704ffd5184a51d0fb820031a
Hailey Francis <n01402670@unf.edu>
2022-01-31 14:46:16 -0500

    Getters/Setters, .csv added, new functions

--- Project1.py
+++ Project1.py
@@ -1,2 +1,53 @@
+"""
+Detailed description of the module.
+
+Author: Hailey Francis
+Version: 1/31/22
+Email: n01402670@unf.edu
+"""
+
+from State import State;
+
+
+def makeList():
+    stateList = []
+    file = open("States.csv", "r")
+    for ln in file:
+        stateVals = ln.split(",")
+        currState = State(stateVals[0], stateVals[1], stateVals[2], stateVals[3], stateVals[4], stateVals[5], stateVals[6], stateVals[7], stateVals[9])
+        stateList.append(currState)
+    del stateList[0] #remove header
+    return stateList
+#print state report
+def printReport(stateList):
+    print(f"{'State' : <15}{'Capitol' : <15}{'Reigon' : <15}{'House Seats' : <15}{'Covid Cases' : <15}{'Covid Deaths' : <15}{'Full Vax Rate' : <15}{'Median House Income' : <15}{'Violent Crime' : <15}")
+    print(stateList[0])
+    
+    
+#sort states by name using quick sort
+
+#sort states by case fatality rate (using Merge sort, 
+#case fatality rate is the rate of deaths over cases)
+
+#find and print a state for a given name 
+#using binary search if sorted by name
+#sequential if not
+
+#print Spearmans p correlation matrix
+
+
 print("CAP4630 Project 1")
 print("Author: Hailey Francis")
+stateList = makeList()
+printReport(stateList)
+
+
+
+
+
+
+
+
+
+
+
---
+++ State.py
@@ -0,0 +1,96 @@
+"""
+Detailed description of the module.
+
+Author: Hailey Francis
+Version: 1/31/22
+Email: n01402670@unf.edu
+"""
+
+class State:
+    # INITIALIZER #
+    def __init__(state, name, capitol, reigon, houseSeats, covidCases, covidDeaths, fullVaxRates, medHouseIncome, violentCrime):
+        state.name = name
+        state.capitol = capitol
+        state.reigon = reigon
+        state.houseSeats = houseSeats
+        state.covidCases = covidCases
+        state.covidDeaths = covidDeaths
+        state.fullVaxRates = fullVaxRates
+        state.medHouseIncome = medHouseIncome
+        state.violentCrime = violentCrime
+    
+    # GETTER METHODS #
+    def getName(state):
+        return state.name
+    
+    def getCapitol(state):
+        return state.capitol
+    
+    def getReigon(state):
+        return state.reigon
+
+    def getHouseSeats(state):
+        return state.houseSeats
+
+    def getCovidCases(state):
+        return state.covidCases
+
+    def getCovidDeaths(state):
+        return state.covidDeaths
+
+    def getFullVaxRates(state):
+        return state.fullVaxRates
+
+    def getMedHouseIncome(state):
+        return state.medHouseIncome
+
+    def getViolentCrime(state):
+        return state.violentCrime
+    # END OF GETTER METHODS #
+
+    # SETTER METHODS #
+    def setName(state, newName):
+        state.name = newName
+
+    def setCapitol(state, newCapitol):
+        state.capitol = newCapitol
+
+    def setReigon(state, newReigon):
+        state.reigon = newReigon
+
+    def setHouseSeats(state, newHouseSeats):
+        state.houseSeats = newHouseSeats
+
+    def setCovidCases(state, newCovidCases):
+        state.covidCases = newCovidCases
+
+    def setCovidDeaths(state, newCovidDeaths):
+        state.covidDeaths = newCovidDeaths
+
+    def setFullVaxRates(state, newFullVaxRates):
+        state.fullVaxRates = newFullVaxRates
+
+    def setMedHouseIncome(state, newMedHouseIncome):
+        state.medHouseIncome = newMedHouseIncome
+
+    def setViolentCrime(state, newViolentCrime):
+        state.violentCrime = newViolentCrime
+    # END OF SETTER METHODS #
+
+
+    #__gt__ and __str__
+    def __str__(state):
+        return f"{state.getName() : <15}{'TEST2' : <15}"
+        """return state.getName() + \
+             "\t" + state.getCapitol() + \
+                "\t" + state.getReigon() + \
+                     "\t" + state.getHouseSeats() + \
+                         "\t" + state.getCovidCases() + \
+                             "\t" + state.getCovidDeaths() + \
+                                 "\t" + state.getFullVaxRates() + \
+                                     "\t" + state.getMedHouseIncome() + \
+                                         "\t" + state.getViolentCrime()
+"""
+
+    def __gt__(state, other):
+        return 
